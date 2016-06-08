fa7d4daf684f1e3314a9a988b33592c9648aae63
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-06-08 16:30:16 +0300

    Add heat-cfn-keystone to required_for in keystone-controller
    Also fix python-openstackclient package install.
    
    Change-Id: Iad6ff64be7e96214332a3519abcbc40c3fe4f863
    Closes-Bug: #1590435

--- deployment_scripts/hiera-override.pp
+++ deployment_scripts/hiera-override.pp
@@ -127,6 +127,6 @@ deploy_vrouter: <%= @deploy_vrouter %>
 
   #FIXME(mattymo): https://bugs.launchpad.net/fuel/+bug/1479317
   package { 'python-openstackclient':
-    ensure => latest,
+    ensure => 'installed',
   }
 }
